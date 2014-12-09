3eb09a493ee7b52eba5ccf9757c5b9c0a8a67224
morrillo <gustavo.orrillo@moldeo.coop>
2014-12-09 15:13:16 -0200

    First working commit

---
+++ sale.py
@@ -0,0 +1,25 @@
+from openerp.osv import fields,osv
+from openerp import tools
+
+ 
+class sale_order_report(osv.osv):
+    _name = "sale.order.report"
+    _description = "Sale Order Report"
+    _auto = False
+    _columns = {
+	'user_id': fields.many2one('res.users','Vendedor'),
+	'product_id': fields.many2one('product.product','Product'),
+	'warehouse_id': fields.many2one('stock.warehouse','Sucursal'),
+	'price_subtotal': fields.float('Total',readonly=True,group_operator="sum",digits=(16,2)), 
+	'product_uom_qty': fields.float('Quantity',readonly=True,group_operator="sum",digits=(16,2)),
+	}
+ 
+    def init(self, cr):
+        tools.sql.drop_view_if_exists(cr, 'sale_order_report')
+	cr.execute("""
+		create or replace view sale_order_report as (
+			select  b.id as id,b.product_id,a.warehouse_id,a.user_id,b.price_unit * b.product_uom_qty as price_subtotal,b.product_uom_qty  
+                        from sale_order a inner join sale_order_line b on a.id = b.order_id where a.state in ('manual','progress'))
+	""")	
+ 
+sale_order_report()
