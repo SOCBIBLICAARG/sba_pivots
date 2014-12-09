c1dbef7a164f6bf3d5ad0d2799880fd90d39bae6
darioleconte <darioleconte@gmail.com>
2014-12-09 15:49:20 -0300

    Cambiamos el warehouse_id por section_id

--- sale.py
+++ sale.py
@@ -9,7 +9,7 @@ class sale_order_report(osv.osv):
     _columns = {
 	'user_id': fields.many2one('res.users','Vendedor'),
 	'product_id': fields.many2one('product.product','Product'),
-	'warehouse_id': fields.many2one('stock.warehouse','Sucursal'),
+	'section_id': fields.many2one('crm.case.section','Sucursal'),
 	'price_subtotal': fields.float('Total',readonly=True,group_operator="sum",digits=(16,2)), 
 	'product_uom_qty': fields.float('Quantity',readonly=True,group_operator="sum",digits=(16,2)),
 	}
@@ -18,7 +18,7 @@ class sale_order_report(osv.osv):
         tools.sql.drop_view_if_exists(cr, 'sale_order_report')
 	cr.execute("""
 		create or replace view sale_order_report as (
-			select  b.id as id,b.product_id,a.warehouse_id,a.user_id,b.price_unit * b.product_uom_qty as price_subtotal,b.product_uom_qty  
+			select  b.id as id,b.product_id,a.section_id,a.user_id,b.price_unit * b.product_uom_qty as price_subtotal,b.product_uom_qty  
                         from sale_order a inner join sale_order_line b on a.id = b.order_id where a.state in ('manual','progress'))
 	""")	
  
